03d920d43af3f9b635c62d5114dc27aec14030fd
Thee5176 <Theerapong5176@gmail.com>
2025-06-28 14:56:41 +0900

    initialize and setup application.properties

---
+++ src/main/java/com/thee5176/record/springboot_cqrs_query/SpringbootCqrsQueryApplication.java
@@ -0,0 +1,13 @@
+package com.thee5176.record.springboot_cqrs_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCqrsQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCqrsQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thee5176/record/springboot_cqrs_query/SpringbootCqrsQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thee5176.record.springboot_cqrs_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCqrsQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
