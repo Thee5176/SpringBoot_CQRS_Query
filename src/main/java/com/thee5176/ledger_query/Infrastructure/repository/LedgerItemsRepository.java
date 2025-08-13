51e2f2c3a8efbead3a60a626bbd81c4695475d96
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-08-13 16:07:35 +0900

    bring back LedgerItemsRepository file

---
+++ src/main/java/com/thee5176/ledger_query/Infrastructure/repository/LedgerItemsRepository.java
@@ -0,0 +1,27 @@
+package com.thee5176.ledger_query.Infrastructure.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+import com.thee5176.ledger_query.Domain.model.Tables;
+import com.thee5176.ledger_query.Domain.model.tables.pojos.LedgerItems;
+import lombok.AllArgsConstructor;
+
+@Repository
+@AllArgsConstructor
+public class LedgerItemsRepository {
+    
+    private final DSLContext dslContext;
+
+    public Optional<LedgerItems> getAllLedgerItems() {
+        return dslContext.selectFrom(Tables.LEDGER_ITEMS)
+                .fetchOptionalInto(LedgerItems.class);
+    }
+
+    public Optional<LedgerItems> getLedgerItemsById(UUID id) {
+        return dslContext.selectFrom(Tables.LEDGER_ITEMS)
+            .where(Tables.LEDGER_ITEMS.ID.eq(id))
+            .fetchOptionalInto(LedgerItems.class);
+    }
+}
